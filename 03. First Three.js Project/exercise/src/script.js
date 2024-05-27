37058868e04794736aa754913117f52a118371a0
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-05-27 23:53:31 +0900

    recap 03

--- 03. First Three.js Project/exercise/src/script.js
+++ 03. First Three.js Project/exercise/src/script.js
@@ -3,14 +3,22 @@ import * as THREE from "three";
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
+// 4 important elements to get started!
+// - A scene that contains
+// - Some objects
+// - A camera
+// - A renderer
+
 // Scene
 const scene = new THREE.Scene();
 
 /**
  * Object
+ *
+ * A Mesh is combination of a geometry(the shape) and a material(how it looks)
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0xd7790f });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -24,6 +32,9 @@ const sizes = {
 
 /**
  * Camera
+ *
+ * The first parameter is the field of view(angle of camera)
+ * Second parameter is the aspect ratio (가로 세로 비율). 왜 필요할까?
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
